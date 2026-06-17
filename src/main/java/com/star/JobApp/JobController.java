08d73d45877b8c25d6c199a93b66f049ab650181
Hardi <hardivirani2003@gmail.com>
2026-06-16 20:09:00 -0400

    vid - 76 : Working with Layers

--- src/main/java/com/star/JobApp/JobController.java
+++ src/main/java/com/star/JobApp/JobController.java
@@ -1,13 +1,21 @@
 package com.star.JobApp;
 
 import com.star.JobApp.model.JobPost;
+import com.star.JobApp.service.JobService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class JobController {
+
+    @Autowired
+    private JobService service; //Created this so, controller and service and talk to each other.
+
+
 
     @GetMapping({"/", "home"})
     public String home() {
@@ -21,6 +29,7 @@ public class JobController {
 
     @PostMapping(value = "handleForm")
     public String handleform(JobPost jobPost) {
+        service.addJob(jobPost);
         return "success";
     }
 
